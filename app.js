d297ba2c3f10040ddf7241164f65a94b748b76d5
143redcrossbaguio-netizen <143redcross.baguio@gmail.com>
2026-03-20 19:25:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuDknEG8pjbT0_yaEDZ77IFYdGYrtwCIE",
+  authDomain: "red-cross-baguio-tti-monitor.firebaseapp.com",
+  projectId: "red-cross-baguio-tti-monitor",
+  storageBucket: "red-cross-baguio-tti-monitor.firebasestorage.app",
+  messagingSenderId: "361015244463",
+  appId: "1:361015244463:web:076f8802942ca904f5065a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
